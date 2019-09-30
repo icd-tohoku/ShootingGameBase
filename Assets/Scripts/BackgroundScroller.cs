ae0fc9ec2eb91171af22e385a3064b573b0e05e5
Isamu Endo <alupaca1363@gmail.com>
2019-09-30 18:45:16 +0900

    add: Background scroll

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BackgroundScroller : MonoBehaviour
+{
+    private Sprite _sprite;
+
+    private void Awake()
+    {
+        _sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+        Debug.Log(_sprite.bounds.size.x);
+    }
+
+    private void Update()
+    {
+        transform.Translate (-0.1f, 0, 0);
+        if (transform.position.x < -_sprite.bounds.size.x * transform.localScale.x ) {
+            transform.position = new Vector3 (_sprite.bounds.size.x * transform.localScale.x, 0, 0);
+        }
+    }
+}
