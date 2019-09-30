0f159887c7bb9aa71198e3b476bf89b09b9dfbdd
Isamu Endo <alupaca1363@gmail.com>
2019-09-30 21:32:05 +0900

    update: プレイヤーを自由に移動できるように

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    /// <summary>
+    /// 毎フレーム呼ばれる処理
+    /// </summary>
+    private void Update()
+    {
+        // 上矢印が押されたら
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            // 上に移動
+            transform.Translate(0.1f * moveSpeed, 0, 0);
+        }
+        
+        // 下矢印が押されたら
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            // 下に移動
+            transform.Translate(-0.1f * moveSpeed, 0, 0);
+        }
+
+        // 右矢印が押されたら
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // 右に移動
+            transform.Translate(0, -0.1f * moveSpeed, 0);
+        }
+        
+        // 左矢印が押されたら
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // 左に移動
+            transform.Translate(0, 0.1f * moveSpeed, 0);
+        }
+    }
+}
