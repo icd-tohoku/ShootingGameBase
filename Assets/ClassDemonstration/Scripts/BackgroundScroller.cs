1951c0c7ad3d068c14b5bbb893b4df2338814998
Isamu Endo <alupaca1363@gmail.com>
2019-12-01 14:29:20 +0900

    update: 背景を動かす動画撮影

---
+++ Assets/ClassDemonstration/Scripts/BackgroundScroller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace MyGame
+{
+    public class BackgroundScroller : MonoBehaviour
+    {
+        public float scrollSpeed;
+        public SpriteRenderer renderer;
+        
+        private void Update()
+        {
+            if (GameData.isPlayable == false)
+            {
+                return;
+            }
+            
+            transform.Translate(-scrollSpeed, 0, 0);
+
+            if (transform.position.x < -renderer.sprite.bounds.size.x * transform.localScale.x)
+            {
+                transform.position = new Vector3(renderer.sprite.bounds.size.x * transform.localScale.x, 0, 0);
+            }
+        }
+
+        public void SetImage(Sprite image)
+        {
+            renderer.sprite = image;
+        }
+    }
+}
---
+++ Assets/ClassDemonstration/Scripts/Test/BackgroundScrollTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame
+{
+    public class BackgroundScrollTest : MonoBehaviour
+    {
+        public BackgroundScroller scroller1;
+        public BackgroundScroller scroller2;
+
+        public Sprite image1;
+        public Sprite image2;
+        
+        private void Start()
+        {
+            GameData.isPlayable = true;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                scroller1.SetImage(image1);
+                scroller2.SetImage(image2);
+            }
+        }
+    }
+}
